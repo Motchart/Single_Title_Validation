54192c0cec67ff3aed0a1f43735c5a6359e0272e
Vasily Lukashuk <motchart007@gmail.com>
2016-11-07 20:12:02 -0800

    Java Single Argument

--- src/main/java/core/HtmlUnit.java
+++ src/main/java/core/HtmlUnit.java
@@ -6,13 +6,15 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
 public class HtmlUnit {
-	public static void main(String[] args) {
+	public static void main(String[] v) {
 
 		WebDriver driver = new HtmlUnitDriver();
 		String text_case_id = "TC-002.01";
-		String url = "http://www.learn2test.net";
-		String title_expected = "learn2tes.net";
-		
+		//String url = "http://www.learn2test.net";
+		//String title_expected = "learn2tes.net";
+		String param[] = v[0].split("\\|");
+		String url = param[0];
+		String title_expected = param[1];
 		driver.get(url);
 		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
 		String title_actual = driver.getTitle();
